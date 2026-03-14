59df002f3f9ebf483cae26725a08415f1462cc25
McChiquito <alicyahir02@gmail.com>
2026-03-13 20:34:58 -0600

    agregue una busqueda por especialidad ne este caso busqueda de especialidad en HTML

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 const employees = [
-    { id: 1, name: 'Jhon Doe', age: 30, departament: 'IT', salary: 50000},
-    { id: 2, name: 'Alice Smith', age: 28, departament: 'HR', salary: 4500},
-    { id: 3, name: 'Bob Johnson', age: 35, departament: 'FInance', salary: 60000},
-    { id: 4, name: 'Yahir Zaele', age: 23, departament: 'Developmen', salary: 30000},
+    { id: 1, name: 'Jhon Doe', age: 30, departament: 'IT', salary: 50000, specialization: 'JavaScript'},
+    { id: 2, name: 'Alice Smith', age: 28, departament: 'HR', salary: 4500, specialization: 'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, departament: 'FInance', salary: 60000, specialization: 'Java'},
+    { id: 4, name: 'Yahir Zaele', age: 23, departament: 'Developmen', salary: 30000, specialization: 'HTML'},
 ];
 
 // Función para mostrar todos los empleados
@@ -31,5 +31,15 @@ function findEmployeeById(employeeId) {
     }
     else {
         document.getElementById('employeeDetails').innerHTML = 'no se ha encontrado ningun empleado con esta Id'
+    }
+}
+
+function findBySpecialization() {
+    const foundEmployee = employees.find(emp => emp.specialization === 'HTML');
+    if(foundEmployee){
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.departament}: ${foundEmployee.specialization} - $${foundEmployee.salary}</p>`;        
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'No se econtraron especialistas en ese lenguaje'
     }
 }
