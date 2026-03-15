662cc59d33220d51b2aab78e2feb550d80908d56
McChiquito <alicyahir02@gmail.com>
2026-03-15 03:17:12 -0600

    Creacios de Sistema de Gestios de Libros, Falta arregrar bugs

---
+++ bookSystem/book_system.js
@@ -0,0 +1,50 @@
+let books = [];
+
+function addBook() {
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDEscription').value;
+    const pagesNumber = parseInt(document.getElementById('pagesNumber').value); 
+    
+    if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+        const book = {
+            name: bookName,
+            authorName: authorName,
+            bookDescription: bookDescription,
+            pagesNumber: pagesNumber
+        };
+        book.push(book);
+        showbooks();
+        clearInputs();
+    } else {
+        alert('Por favor, completr todos los campos correctamente');
+    }
+}
+
+function showbooks() {
+    const booksDiv  = books.map((book, index) => `<h1> Numero de libro: ${index + 1}</h1>
+        <p><strong>Nombre del libro: </strong> ${book.name}</p>
+        <p><strong>Nombre del autor: </strong> ${book.authorName}</p>
+        <p><strong>Descripcion del libro: </strong> ${book.bookDescription}</p>
+        <p><strong>Numero de paginas: </strong> ${book.pagesNumber} pagina(s)</p>
+        <button onclick="editbook(${index})">Editar</button>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function editbook(index) {
+    const book = books[index];
+    document.getElementById('bookName').value = book.name;
+    document.getElementById('authorName').value = book.authorName;
+    document.getElementById('bookDescription').value = book.bookDescription;
+    document.getElementById('pagesNumber').value = book.pagesNumber;
+    book.splice(index, i); // Elimina las estradas antiguas
+    showbooks(); // Actualiza la lista
+}
+
+function clearInpouts() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDEscription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
