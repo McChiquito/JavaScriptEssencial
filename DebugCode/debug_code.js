c9976016df41af33025bca89dfffa6dd9ee9fb16
McChiquito <alicyahir02@gmail.com>
2026-03-16 01:47:50 -0600

    Agregue operacion de suma y resta con sus propios resultados

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -5,9 +5,13 @@ function performOperation() {
     // Check if inputs are valid numbers
     if(!isNaN(num1) && !isNaN(num2)) {
     // Perform the operation
-        let result = multiply(num1, num2);
+        let result2 = suma(num1, num2);
+        displayResult2(result2);
+        let result3 = resta(num1, num2);
+        displayResult3(result3);
+        let result1 = multiply(num1, num2);
         // Display the result
-        displayResult(result);
+        displayResult(result1);
     } else {
         displayResult('Please enter valid numbers');
     }
@@ -19,8 +23,28 @@ function performOperation() {
         return a * b;
     }
 
-    function displayResult(result) {
+    function suma(a, b) {
+        debugger;
+        return a + b;
+
+    }
+    function resta(a, b) {
+        debugger;
+        return a - b;
+    }
+
+    function displayResult(result1) {
         // Display the result in the paragraph element
-        const resultElement = document.getElementById('result');
-        resultElement.textContent = `The result is: ${result}`;
+        const resultElement = document.getElementById('result1');
+        resultElement.textContent = `The result is: ${result1}`;
+    }
+
+    function displayResult2(result2) {
+        const resultElement = document.getElementById('result2');
+        resultElement.textContent = `El resultado de tu suma es: ${result2}`;
+    }
+
+    function displayResult3(result3) {
+        const resultElement = document.getElementById('result3');
+        resultElement.textContent = `El resultado de tu resta es: ${result3}`;
     }
